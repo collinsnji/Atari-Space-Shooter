474096c0d2b30f2dc98f8e69b0ab13c2a31a46e7
collinsnji <collins.geek@gmail.com>
2018-03-28 14:01:07 -0400

    Made it cool

--- public/main.js
+++ public/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "FHS Chat v1.0 ";
     log(message, {
       prepend: true
     });
